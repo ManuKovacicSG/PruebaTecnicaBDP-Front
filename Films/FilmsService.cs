21fdf7a5b7d678b41bab051fef438afab76eb141
ManuKovaSG <kovacicmanu@gmail.com>
2023-06-28 16:28:48 +0200

    Bringing Service inside the project

---
+++ Films/FilmsService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.model
+{
+    public class FilmsService : FilmsInterface
+    {
+        private readonly HttpClient _httpClient;
+        const string _baseUrl = "http://albertbdp.somee.com/api/peliculas";
+        const string _filmsEndpoint = "http://albertbdp.somee.com/api/peliculas";
+        const string _host = "albertbdp.somee.com";
+
+
+        public FilmsService(HttpClient httpClient) => _httpClient = httpClient;
+
+
+        public async Task<List<FilmItem>> GetFilms(List<FilmItem> filmItem)
+        {
+            ConfigureHttpClient();
+
+            var response = await _httpClient.GetAsync(_filmsEndpoint);
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            var dto = await JsonSerializer.DeserializeAsync<FilmsDto>(stream);
+            return dto.data.FirstOrDefault().name.Select(n => new FilmItem 
+            { 
+                Name = n.name,
+                Gendre = n.Gendre,
+            }).ToList();
+
+        }
+
+        private void ConfigureHttpClient()
+        {
+            _httpClient.BaseAddress = new Uri(_baseUrl);
+            _httpClient.DefaultRequestHeaders.Add("Films", _host);
+        }
+    }
+}
---
+++ Films/film.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.model
+{
+    public class FilmItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string PosterUrl { get; set; }
+        public object Gendre { get; set; }
+        public object Character { get; internal set; }
+    }
+}
