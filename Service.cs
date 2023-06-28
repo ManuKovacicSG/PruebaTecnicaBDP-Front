88adb8135d559cfd98fd7a35a81f951c979e3446
ManuKovaSG <kovacicmanu@gmail.com>
2023-06-28 16:03:48 +0200

    Add project files.

---
+++ Service.cs
@@ -0,0 +1,13 @@
+﻿namespace PruebaTecnicaBDP
+{
+    public class Service
+    {
+        private HttpClient httpClient;
+
+        public Service(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+    }
+}
